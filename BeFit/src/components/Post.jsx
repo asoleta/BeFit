bca7c47c200653d4c54cca869c69b81d9c97b2e5
Alex Soleta <alexandrasoleta@gmail.com>
2024-11-12 22:31:45 -0500

    Pass props to Post

--- BeFit/src/components/Post.jsx
+++ BeFit/src/components/Post.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Post = () => {
+const Post = ({title, img_url, caption}) => {
     return (
         <div class="wrapper">
             <div class="post">
                 <div class="content">
-                <h3>Title</h3>
-                <img src="" alt="Post Image" />
-                <p>This is where the caption will go.</p>
+                <h3>{title}</h3>
+                <img src={img_url}/>
+                <p>{caption}</p>
                 </div>
             </div>
         </div>
