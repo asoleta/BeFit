0f057452757b87a401bc05dbdedae751c1eaf584
Alex Soleta <alexandrasoleta@gmail.com>
2024-11-14 21:13:11 -0500

    Remove caption from Homepage

--- BeFit/src/components/Post.jsx
+++ BeFit/src/components/Post.jsx
@@ -2,15 +2,6 @@ import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 
 const Post = ({ title, img_url, caption, likes, id, date }) => {
-    const [isExpanded, setIsExpanded] = useState(false);
-
-    // Truncate caption to 300 characters and add ellipsis if necessary
-    const truncatedCaption = caption.length > 300 ? caption.slice(0, 300) + '...' : caption;
-
-    // Toggle the expanded state to show the full caption or truncated one
-    const toggleCaption = () => {
-        setIsExpanded(!isExpanded);
-    };
 
     return (
         <div className="wrapper">
@@ -19,12 +10,6 @@ const Post = ({ title, img_url, caption, likes, id, date }) => {
                 <div className="content">
                     <h3>{title}</h3>
                     <img src={img_url} alt={title} />
-                    <p>{isExpanded ? caption : truncatedCaption}</p>
-                    {caption.length > 300 && (
-                        <button className="expandBtn" onClick={toggleCaption}>
-                            {isExpanded ? 'Show Less' : 'Show More'}
-                        </button>
-                    )}
                     <p>{`Likes: ${likes}`}</p>
                     <p>{`Date Created: ${date}`}</p>
                 </div>
